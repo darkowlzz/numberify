1ebe74a75a328cc98db0bfc2b145622544174059
sunny <indiasuny000@gmail.com>
2014-11-10 23:31:34 +0530

    numerify_data

--- numberify/numberify.py
+++ numberify/numberify.py
@@ -17,31 +17,59 @@
 from sys import argv, exc_info
 
 
-def numberify(filename):
-    '''Prepend each line of a file with the line number
-    Argument:
-    filename - name of the file to be numberified
-    '''
-    try:
-        fd = open(filename, 'r+')
-        lines = fd.readlines()
-        for i in range(len(lines)):
-            lines[i] = str(i+1) + ' ' + lines[i]
+class Numberify():
 
-        fd.seek(0)
-        fd.writelines(lines)
-        fd.close()
-        return True
-    except IOError as e:
-        print "I/O error({0}): {1}".format(e.errno, e.strerror)
-    except:
-        print 'Unexpected error:', exc_info()[0]
-        raise
+    def __init__(self):
+        pass
+
+    def numberify_file(self, filename):
+        '''Prepend each line of a file with the line number
+        Argument:
+        filename - name of the file to be numberified
+        '''
+        try:
+            fd = open(filename, 'r+')
+            lines = fd.readlines()
+            for i in range(len(lines)):
+                lines[i] = str(i+1) + ' ' + lines[i]
+            fd.seek(0)
+            fd.writelines(lines)
+            fd.close()
+            return True
+        except IOError as e:
+            print 'I/O error({0}): {1}'.format(e.errno, e.strerror)
+        except:
+            print 'Unexpected error:', exc_info()[0]
+            raise
+
+    def numberify_data(self, source, start=1):
+        '''Return a dictionary with numberified data
+        Arguments:
+        source -- source of data(filename or a list)
+        start  -- starting index of numbering
+        '''
+        if type(source) is str:
+            try:
+                fd = open(source, 'r+')
+                data = fd.readlines()
+                for index, item in enumerate(data):
+                    data[index] = item.strip('\n')
+                fd.close()
+            except IOError as e:
+                print 'I/O error({0}): {1}'.format(e.errno, e.strerror)
+                return False
+        elif type(source) is list:
+            data = source
+        else:
+            print 'Data error. Pass a filename or a list'
+            return False
+        return dict(list(enumerate(data, start)))
 
 
 if __name__ == '__main__':
     if len(argv) == 2:
         filename = argv[1]
-        numberify(filename)
+        nfy = Numberify()
+        nfy.numberify_file(filename)
     else:
         print 'Usage: numberify <filename>'
--- test/test_numberify.py
+++ test/test_numberify.py
@@ -1,18 +1,36 @@
 from shutil import copyfile
-from os import remove
-from numberify.numberify import numberify
+from os import remove, path
+from numberify.numberify import Numberify
 
-file1 = 'test/foo.list'
-file2 = 'test/bar.list'
+this_dir, this_filename = path.split(__file__)
+file1 = path.join(this_dir, 'foo.list')
+file2 = path.join(this_dir, 'bar.list')
+testData = ['pen', 'ruler', 'paper', 'ink']
 
 
-def test():
-    '''testing numberify'''
+def test_numberify_file():
+    '''testing numberify_file'''
+    num = Numberify()
     copyfile(file1, file2)
-    numberify(file2)
+    num.numberify_file(file2)
     fd = open(file2, 'r')
     lines = fd.readlines()
     length = len(lines)
     assert '1' in lines[0]
     assert str(length) in lines[length - 1]
     remove(file2)
+
+
+def test_numberify_data():
+    '''testing numberify_data'''
+    num = Numberify()
+    result1 = num.numberify_data(testData)
+    assert result1[1] == 'pen'
+    assert result1[4] == 'ink'
+    result2 = num.numberify_data(file1)
+    assert result2[1] == 'balbasaur'
+    assert result2[6] == 'pidgeotto'
+    result3 = num.numberify_data('notexistingfile')
+    assert result3 is False
+    result4 = num.numberify_data({1: 'foo'})
+    assert result4 is False
